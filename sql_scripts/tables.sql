ca69483e67c5c157d9dbb1376f1cd68b28a535e1
Bhuvi2 <bhuvialluri@gmail.com>
2023-04-20 19:14:44 -0400

    fix: updated tables.sql

--- sql_scripts/tables.sql
+++ sql_scripts/tables.sql
@@ -6,3 +6,11 @@ create table users (
     passwords varchar(20) not null,
     primary key(id)
 );
+
+CREATE TABLE posts (
+	id int not null auto_increment,
+    profile_username varchar(20) not null,
+    written_text varchar(200) not null,
+    media_location varchar(300) not null,
+    primary key(id)
+);
